8e5e0d8d25997303e823574b0824303339e3e65c
Luciana <luciana.soares@6place.com.br>
2022-09-02 15:22:23 -0300

    feat: CRUD for Users implemented

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,88 @@
 const database = require("../../api/models");
 
 class UserController {
+  /**
+   * @returns A JSON with all users in database if success. If fail, returns message error.
+   */
 
-/**
- * @returns A JSON with all users in database if success. If fail, returns message error.
- */
-  
   static async getUsers(req, res) {
     try {
-      const response = await database.users.findAll();
-      return res.status(200).json(response);
+      const databaseMethod = await database.users.findAll();
+      return res.status(200).json(databaseMethod);
     } catch (err) {
       return res.status(404).json(err.message);
+    }
+  }
+
+  /**
+   * @returns A JSON with a specific user in database if success. If fail, returns message error.
+   */
+
+  static async getUserById(req, res) {
+    const { id } = req.params;
+    try {
+      const databaseMethod = await database.users.findOne({
+        where: {
+          id: id,
+        },
+      });
+      return res.status(200).json(databaseMethod);
+    } catch (err) {
+      return res.status(404).json(err.message);
+    }
+  }
+
+  static async createUser(req, res) {
+    const data = req.body;
+    try {
+      const databaseMethod = await database.users.create(data);
+      return res.status(200).json(databaseMethod);
+    } catch (err) {
+      return res.status(404).json(err.message);
+    }
+  }
+
+  /**
+   * @returns A message informing that the the user was updated and a JSON with informations about the updated user if success. If fail, returns message error.
+   */
+
+  static async updateUser(req, res) {
+    const { id } = req.params;
+    const data = req.body;
+    try {
+      await database.users.update(data, {
+        where: {
+          id: id,
+        },
+      });
+      const getUpdatedUser = await database.users.findOne({
+        where: {
+          id: id,
+        },
+      });
+      return res
+        .status(200)
+        .json({ message: "User updated.", user: getUpdatedUser });
+    } catch (err) {
+      return res.status(404).json(err.message);
+    }
+  }
+
+  /**
+   * @returns A success deletion message if success. If fail, returns message error.
+   */
+
+  static async deleteUser(req, res) {
+    const { id } = req.params;
+    try {
+      await database.users.destroy({
+        where: {
+          id: id,
+        },
+      });
+      return res.status(200).json({ message: `User with id ${id} deleted` });
+    } catch (err) {
+      return res.status(500).json(err.message);
     }
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ const UserController = require("../controllers/UserController");
 const router = Router()
 
 router.get('/', UserController.getUsers)
+router.get('/:id', UserController.getUserById)
+router.post('/', UserController.createUser)
+router.put('/:id', UserController.updateUser)
+router.delete('/:id', UserController.deleteUser)
 
 module.exports = router
